f11bd27b602dc8a4619c263407a24a3469444e87
lserafin <luzianserafin@gmail.com>
2020-01-09 10:44:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-8b8yyq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-8b8yyq)
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+// Import Vue
+import Vue from 'vue';
+
+// Write Javascript code!
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
